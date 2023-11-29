ba9224e6bf962ef693113876824c6f17a9763935
Aavild <anders@aavild.dk>
2023-11-29 01:35:28 +0100

    build and ConfigManager.cs

---
+++ Config/ConfigManager.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx.Configuration;
+using BepInEx.Logging;
+
+namespace NewTerminal.Config;
+
+public class ConfigManager
+{
+    private ConfigEntry<string> configGreeting;
+    private ConfigEntry<bool> configDisplayGreeting;
+
+    internal void LoadConfig()
+    {
+        configGreeting = Plugin.Instance.Config.Bind("General",      // The section under which the option is shown
+            "GreetingText",  // The key of the configuration option in the configuration file
+            "Hello, world!", // The default value
+            "A greeting text to show when the game is launched"); // Description of the option to show in the config file
+        
+        configDisplayGreeting = Plugin.Instance.Config.Bind("General.Toggles", 
+            "DisplayGreeting",
+            true,
+            "Whether or not to show the greeting text");
+        // Test code
+        Plugin.Log.LogInfo("Hello, world!");
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -1,14 +1,31 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using NewTerminal.Config;
+using UnityEngine;
 
 namespace NewTerminal
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInProcess("Lethal Company.exe")]
     public class Plugin : BaseUnityPlugin
     {
+        internal static Plugin Instance;
+        internal static ManualLogSource Log;
         private void Awake()
         {
+            Log = Logger;
+            Instance = this;
             // Plugin startup logic
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            
+            Logger.LogInfo("This is information");
+            Logger.LogWarning("This is a warning");
+            Logger.LogError("This is an error");
+
+            ConfigManager config = new ConfigManager();
+            config.LoadConfig();
         }
     }
 }
