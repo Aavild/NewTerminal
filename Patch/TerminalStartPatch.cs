cea1221ce2eff2a2b62823c876826932e177fc61
Aavild <anders@aavild.dk>
2023-12-01 00:26:35 +0100

    Fixed some keywords wrongfully being combined and split terminalText into multiple config files. Added CHANGELOG.md

--- Patch/TerminalStartPatch.cs
+++ Patch/TerminalStartPatch.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using BepInEx.Configuration;
 using HarmonyLib;
+using UnityEngine;
 
 namespace NewTerminal.Patch;
 /// <summary>
@@ -10,16 +12,21 @@ public class TerminalStartPatch
 {
     static bool Prefix(Terminal __instance)
     {
+        var specialConfigFile = CreateNewConfig("NewTerminal-Special");
+        var verbsConfigFile = CreateNewConfig("NewTerminal-Verbs");
+        var otherConfigFile = CreateNewConfig("NewTerminal-Other");
+        
+        // __instance.terminalNodes.specialNodes
         foreach (TerminalNode node in __instance.terminalNodes.specialNodes)
-            ConfigTerminalMenu("TerminalMenus.Special", node.name, ref node.displayText);
+            ConfigTerminalMenu(specialConfigFile, "TerminalMenus.Special", node.name, ref node.displayText);
 
-        // __instance.terminalNodes.allKeywords are super weird
+        // __instance.terminalNodes.allKeywords
         foreach (TerminalKeyword keyword in __instance.terminalNodes.allKeywords)
         {
             if (keyword.isVerb)
             {
                 foreach (CompatibleNoun noun in keyword.compatibleNouns)
-                    ConfigTerminalMenu("TerminalMenus.Nouns", noun.noun.name, ref noun.result.displayText);
+                    ConfigTerminalMenu(verbsConfigFile, $"TerminalMenus.{keyword.name}", $"{noun.noun.name}", ref noun.result.displayText);
                 
                 continue;
             }
@@ -32,7 +39,7 @@ public class TerminalStartPatch
                 continue;
             }
             
-            ConfigTerminalMenu("TerminalMenus.Other", keyword.name, ref keyword.specialKeywordResult.displayText);
+            ConfigTerminalMenu(otherConfigFile, "TerminalMenus.Other", keyword.name, ref keyword.specialKeywordResult.displayText);
             
         }
         
@@ -41,29 +48,42 @@ public class TerminalStartPatch
         return true;
     }
 
+    
+    private static readonly string DefaultPath = Plugin.Instance.Config.ConfigFilePath;
+    private static ConfigFile CreateNewConfig(string fileName)
+    {
+        return
+            new ConfigFile(
+                DefaultPath.Replace("NewTerminal", fileName, StringComparison.OrdinalIgnoreCase), false,
+                Plugin.InstanceDefinition);
+    }
+
     /// <summary>
     /// Creates or uses a config entry for a given terminal element.
     /// </summary>
+    /// <param name="file"></param> the ConfigFile to save to
     /// <param name="section"></param> The section under which the option is shown
     /// <param name="name"></param> The key of the configuration option in the configuration file
     /// <param name="displayText"></param> The text to be configured
-    private static void ConfigTerminalMenu(string section, string name, ref string displayText)
+    private static void ConfigTerminalMenu(ConfigFile file, string section, string name, ref string displayText)
     {
         try
         {
-            var configEntry = Plugin.Instance.Config.Bind(
+            var configEntry = file.Bind(
                 section,
                 name,
                 displayText, 
                 "");
-            if (configEntry.Value.Length > 240)
-                throw new OverflowException(
-                    "Terminal Message can't be longer than 240 letters");
+            if (configEntry.Value.Length > 150000)
+            {
+                Plugin.Log.LogError($"{name} has a length beyond 150.000 and will be skipped");
+                return;
+            }
             displayText = configEntry.Value;
         }
-        catch (Exception e) //IOException, Overflow, displayText null
+        catch (Exception e) //IOException, displayText null
         {
-            Plugin.Log.LogError($"Error loading {name} in {section}");
+            Plugin.Log.LogError($"Error loading {name} in {section} in {file.ConfigFilePath}");
             Plugin.Log.LogError(e);
         }
     }
